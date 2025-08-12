1ce46eff6d1afeadb125ef913b81d5270bfc883d
jmcc-137 <jmcrispinc20@gmail.com>
2025-08-12 12:27:37 -0500

    style: :lipstick: se mejoro la impresion en consola

--- src/Modules/Variedad/Application/Services/VariedadShowCatalogoService.cs
+++ src/Modules/Variedad/Application/Services/VariedadShowCatalogoService.cs
@@ -25,9 +25,17 @@ namespace Cafe_Colombiano.src.Modules.Variedad.Application.Services
                 {
                     Console.WriteLine("".PadRight(80, '='));
                     Console.WriteLine($"ID: {u.id}, Nombre: {u.nombre_comun} ({u.nombre_cientifico})\n");
-                    Console.WriteLine($"{u.imagen_referencia_url}\n");
-                    Console.WriteLine($"Descripción: {u.descripcion_general}\n");
 
+                    Console.WriteLine($"Imagen: {u.imagen_referencia_url}\n");
+                    Console.WriteLine($"Descripción: {u.descripcion_general}\n");
+                    Console.WriteLine($"Historia del linaje: {u.historia_linaje}\n");
+                    Console.WriteLine($"Grupo Genético: {u.GrupoGenetico?.nombre_grupo}\n");
+                    Console.WriteLine($"Porte: {u.Porte?.nombre_porte}\n");
+                    Console.WriteLine($"Tamaño de Grano: {u.TamanoGrano?.nombre_tamano}\n");
+                    Console.WriteLine($"Altitud Óptima: {u.AltitudOptima?.descripcion}\n");
+                    Console.WriteLine($"Potencial de Rendimiento: {u.PotencialRendimiento?.nivel_rendimiento}\n");
+                    Console.WriteLine($"Calidad de Grano: {u.CalidadGrano?.nivel_calidad}\n");
+                    Console.WriteLine($"Información Agronómica: {u.InformacionAgronomica?.densidad_siembra}\n");
                 }
 
             }
